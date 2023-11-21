849cd161cb7fd1ec32208d78d787646313411bd4
Marzmans <marzan.mk@gmail.com>
2023-11-21 18:33:31 -0500

    Create Consolidation New.py

---
+++ Consolidation New.py
@@ -0,0 +1,16 @@
+import sys
+#create step
+#step = int(sys.argv[1])
+#user_str = sys.argv[2]
+
+def caesar(input = sys.argv[1],step = sys.argv[2]):
+    
+    
+
+
+for arg in sys.argv[1:]:
+    str_list.append(arg)
+    
+#for sysarg in range(len(1, sys.argv)): #used online documentation to use len to skip the script name when iterating over standard args
+ #   #print ("sys.argv[{}]: {}".format(sysarg, sys.argv[sysarg]))
+  #  sysarg[1]
