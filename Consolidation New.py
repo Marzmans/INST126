5953e5bf6ca798def6b0b43f63bf24d56eef3bec
Marzmans <marzan.mk@gmail.com>
2023-11-24 23:46:27 -0500

    Update Consolidation New.py

--- Consolidation New.py
+++ Consolidation New.py
@@ -1,16 +1,33 @@
 import sys
-#create step
-#step = int(sys.argv[1])
-#user_str = sys.argv[2]
 
-def caesar(input = sys.argv[1],step = sys.argv[2]):
-    
-    
+#decrypt or encrypt the string(s)
+def shift(character, step, encrypt=True):
+    if character.isalpha(): #learned from https://www.w3schools.com/python/ref_string_isalpha.asp
+        original = ord("A") if character.isupper() else ord("a") #Learned to set alphabet at A from a more experienced friend outside of class and learned from https://www.w3schools.com/python/ref_func_ord.asp
+        change = ord(character) - original
+        if encrypt:
+            shifted_offset = (change + step) % 26
+        else:
+            shifted_offset = (change - step + 26) % 26 #I had issues making sure my code works for all letters and wrapping around the alphabet, so I talked to one of my friends outside of class and he recommended mod 26
+        return chr(shifted_offset + original) - #learned from https://www.programiz.com/python-programming/methods/built-in/chr
+    return character
 
+#concatenate the encrypted/decrypted characters
+def caesar(input, step, encrypt=True):
+    return "".join(shift(character, step, encrypt) for character in input) #learned from https://stackoverflow.com/questions/12453580/how-to-concatenate-join-items-in-a-list-to-a-single-string
 
-for arg in sys.argv[1:]:
-    str_list.append(arg)
-    
-#for sysarg in range(len(1, sys.argv)): #used online documentation to use len to skip the script name when iterating over standard args
- #   #print ("sys.argv[{}]: {}".format(sysarg, sys.argv[sysarg]))
-  #  sysarg[1]
+#main section of code to combine other functions to achieve desired outcome
+def main():
+    try:
+        mode = sys.argv[1].lower()
+        if mode not in ["encrypt", "decrypt"]: #troubleshooting if someone does not provide the correct inputs
+            raise ValueError("Inout encrypt or decrypt")
+        step = int(sys.argv[2])
+        strings = sys.argv[3:]
+        for i in strings:
+            result = caesar(i, step, encrypt=(mode == "encrypt"))
+            print("{}ed: {}.".format(sys.argv[1], result))
+    except (ValueError, IndexError): #learned how to handle multiple possible errors from https://rollbar.com/blog/python-catching-multiple-exceptions/
+        print("Run the program like this: python script.py (encrypt/decrypt) (step) (string1) (string2) ...")
+
+main()
