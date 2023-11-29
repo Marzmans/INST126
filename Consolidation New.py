8ffef29a48a1b1ed7ab5bb95c0ff084077921c21
Marzmans <marzan.mk@gmail.com>
2023-11-29 18:58:49 -0500

    Checklist updates
    Added better dictionary usage and append/write new files to save the data.

--- Consolidation New.py
+++ Consolidation New.py
@@ -1,7 +1,9 @@
 """Documentation: run program like python, make sure to have Consolidation_functions in the same directory
  Consolidation New.py" (encrypt/decrypt) (step) (string1) (string2)"""
 import sys
+import os
 import Consolidation_functions
+
 
 #main section of code to combine other functions to achieve desired outcome
 
@@ -24,12 +26,20 @@ def main():
             result = Consolidation_functions.caesar(input, step, encrypt=(final_type == "encrypt"))
             #storing original and changed strings
             both[input] = { #Learned how to index/search a value in dictionary from Google GenAI when you search topics
-                "Original String: ": input,
+                #"Original String: ": input,
                 "{}ed String: ".format(sys.argv[1].title()): result 
             }
-
-            #print("{}ed: {}.".format(sys.argv[1], result))
+            #store data in txt file
+            file_path = str(os.getcwd) + "\\" + "stored_data.txt"
+        
             print((both))
+
+        if os.path.exists(file_path):
+                with open("stored_data.txt", "a") as data:
+                    data.write(both)
+        else:
+            with open("stored_data.txt","w") as data:
+                data.write(str(both))
 
     except (ValueError, IndexError): #learned how to handle multiple possible errors from https://rollbar.com/blog/python-catching-multiple-exceptions/
         print(r'Run the program like this: python "Consolidation New.py" (encrypt/decrypt) (step) (string1) (string2) ...')
